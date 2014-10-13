b748cd8fa102e4c0d88d231f3ed99989861768b0
Gwenaël Mahé <gwmahe@live.fr>
2014-10-13 13:44:16 +0200

    Ajout fichiers HTML et CSS

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>...</title>
+		<link href='normalize.css' rel='stylesheet'>
+		<link href='index.css' rel='stylesheet'>
+	</head>
+	<body>
+		<header>
+			<h1>Dephdre.</h1><a href='#'>Accueil</a><a href='#'>Projets</a><a href='#'>Ressources</a><a href='#'>Contacts</a>
+		</header>
+		<section>
+			<article>
+				<h4>NOUVELLE NOUVELLE !</h4><span class='date'>3 octobre 2014 - 17:29:50</span>
+					<p>Integer justo nisi, sollicitudin vitae diam quis, ullamcorper tempus leo. Fusce vulputate metus et est ornare, sed elementum leo pretium. Integer tristique nunc vulputate cursus posuere. Quisque erat ante, placerat et odio quis, tempus ornare tortor. Curabitur fringilla diam id sapien euismod, at efficitur metus varius. Nullam tempus ex nec urna laoreet, nec mattis metus laoreet. Pellentesque ut rutrum enim. Cras sed augue sed sem lacinia posuere. Maecenas hendrerit, tortor at sollicitudin bibendum, quam nibh vulputate sem, vel mattis dui risus non odio. Duis non ligula at elit tempus auctor ut vitae velit. Nullam porttitor varius sapien, et elementum massa vehicula ut. Cras consectetur vestibulum est non dignissim. Cras feugiat sem in enim commodo tempus. Pellentesque nibh felis, lobortis et mauris non, pretium faucibus ex. Morbi eu nulla sagittis, porta nunc id, faucibus neque.</p>
+			</article>
+			<article class='article_bas_de_page'>
+				<h4>NOUVELLE NOUVELLE !</h4><span class='date'>3 octobre 2014 - 17:32:50</span>
+					<p>Integer justo nisi, sollicitudin vitae diam quis, ullamcorper tempus leo. Fusce vulputate metus et est ornare, sed elementum leo pretium. Integer tristique nunc vulputate cursus posuere. Quisque erat ante, placerat et odio quis, tempus ornare tortor. Curabitur fringilla diam id sapien euismod, at efficitur metus varius. Nullam tempus ex nec urna laoreet, nec mattis metus laoreet. Pellentesque ut rutrum enim. Cras sed augue sed sem lacinia posuere. Maecenas hendrerit, tortor at sollicitudin bibendum, quam nibh vulputate sem, vel mattis dui risus non odio. Duis non ligula at elit tempus auctor ut vitae velit. Nullam porttitor varius sapien, et elementum massa vehicula ut. Cras consectetur vestibulum est non dignissim. Cras feugiat sem in enim commodo tempus. Pellentesque nibh felis, lobortis et mauris non, pretium faucibus ex. Morbi eu nulla sagittis, porta nunc id, faucibus neque.</p>
+			</article>
+		</section>
+		<footer>
+			<p>Cette création est mise à disposition sous une licence <a href='#'>Creative Commons by-nc-sa</a>.</p>
+		</footer>
+	</body>
+</html>
